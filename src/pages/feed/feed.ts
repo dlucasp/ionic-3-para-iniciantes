d525ae0d771a5cbe5a547398a510205f57d92507
dlucasp <dl1808@hotmail.com>
2018-05-08 15:38:20 -0300

    first commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+  public nome_usuario:string =  "Daniel Lucas do Codigo";
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumero(num1:number, num2:number){
+    //alert(num1 + num2);
+  }
+
+  ionViewDidLoad() {
+   // this.somaDoisNumero(10,20);
+    console.log('ionViewDidLoad FeedPage');
+  }
+
+}
